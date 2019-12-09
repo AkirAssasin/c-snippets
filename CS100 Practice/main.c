4f12f59393730cfd0bcf83c9398401c1cdb2d181
CZ <akirassasindev@gmail.com>
2019-12-09 15:38:58 +0800

    Decimal-9-bit-IEEE754 tests
    this was pretty hard

--- CS100 Practice/main.c
+++ CS100 Practice/main.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
-#define TEST_COUNT 21
+#define TEST_COUNT 23
+#define FLOAT_EPSILON 0.00001f
 
 typedef unsigned char byte;
 typedef signed char sbyte;
+typedef unsigned int quad;
 
 int BinaryToOctalTest (void);
 int OctalToBinaryTest (void);
@@ -39,6 +42,9 @@ int FromTwosComplementTest (void);
 
 int TwosComplementAdditionTest (void);
 
+int DecimalToIEEEByteTest (void);
+int IEEEByteToDecimalTest (void);
+
 int (*const tests[TEST_COUNT])(void) = {
     BinaryToOctalTest,
     OctalToBinaryTest,
@@ -60,7 +66,9 @@ int (*const tests[TEST_COUNT])(void) = {
     FromOnesComplementTest,
     ToTwosComplementTest,
     FromTwosComplementTest,
-    TwosComplementAdditionTest
+    TwosComplementAdditionTest,
+    DecimalToIEEEByteTest,
+    IEEEByteToDecimalTest
 };
 
 int main (void) {
@@ -692,6 +700,219 @@ int TwosComplementAdditionTest (void) {
         printf("Wrong! The answer is ");
     }
     PrintBinaryByte(totalEncoded);
+    printf(".\n");
+
+    return 0;
+
+}
+
+quad RandomIEEEQuad (int _exponent, int _mantissa) {
+
+    /* randomize exponent and mantissa */
+    quad result = rand() % (1 << (_exponent + _mantissa));
+
+    /* shift result all the way back */
+    result <<= 31 - _exponent - _mantissa;
+
+    /* give a random sign */
+    if (RandomFloat() > 0.5f) result |= 0x80000000;
+
+    /* return result */
+    return result;
+
+}
+
+float QuadToFloat (quad _quad, int _exponent, int _mantissa) {
+
+    float result = 1.0f, sign = 10.0f;
+    byte i;
+    int scale = 0;
+
+    /* compute exponent */
+    for (i = 0; i < _exponent; ++i) {
+        quad target = _quad >> (30 - i);
+        if (target % 2) {
+            scale += 1 << (_exponent - 1 - i);
+        }
+    }
+
+    /* apply bias */
+    scale -= (1 << (_exponent - 1)) - 1;
+    if (scale < 0) {
+        scale = -scale;
+        sign = 0.1f;
+    }
+
+    /* compute mantissa */
+    for (i = 0; i < _mantissa; ++i) {
+        quad target = _quad >> (30 - _exponent - i);
+        if (target % 2) {
+            result += 1.0f / (2 << i);
+        }
+    }
+
+    /* apply scale */
+    for (i = 0; i < scale; ++i) {
+        result *= sign;
+    }
+
+    /* apply sign */
+    if ((_quad >> 31) % 2) result = -result;
+
+    return result;
+
+}
+
+int ComputePrecision (quad _quad, int _exponent, int _mantissa) {
+
+    int i, scale = 0;
+
+    /* compute exponent */
+    for (i = 0; i < _exponent; ++i) {
+        quad target = _quad >> (30 - i);
+        if (target % 2) {
+            scale += 1 << (_exponent - 1 - i);
+        }
+    }
+
+    /* apply bias */
+    scale -= (1 << (_exponent - 1)) - 1;
+
+    /* find least significant mantissa */
+    for (i = _mantissa - 1; i >= 0; --i) {
+        quad target = _quad >> (30 - _exponent - i);
+        if (target % 2) {
+            scale -= i + 1;
+            break;
+        }
+    }
+
+    return scale < 0 ? -scale : 0;
+
+}
+
+void PrintBinaryQuad (quad _quad, int _length) {
+
+    /* print out byte in binary form */
+    byte i = 0;
+    while (i < _length) {
+        quad target = _quad >> (31 - i++);
+        printf((target % 2) ? "1" : "0");
+    }
+
+}
+
+int ScanfFloatAnswer (float _check) {
+
+    /* declare variables */
+    float answer = 0;
+    int sr;
+    
+    /* do the scanf */
+    printf("> ");
+    sr = scanf("%f",&answer);
+    ClearScanfBuffer();
+    
+    /* return if correct answer */
+    if (sr != 1 || fabsf(answer - _check) > FLOAT_EPSILON) {
+        return 0;
+    }
+    return 1;
+
+}
+
+int BinaryToQuad (const char *_binary, int _length, quad *const _result) {
+
+    int i = _length;
+    *_result = 0;
+
+    while (i-- && *_binary) {
+        char c = *(_binary++);
+        
+        /* if not binary then go away */
+        if (c != '1' && c != '0') {
+            return 0;
+        }
+
+        /* bit-shift magic */
+        *_result <<= 1;
+        if (c == '1') ++(*_result);
+
+    }
+
+    /* extra shifting */
+    *_result <<= 33 - (_length - i);
+
+    return 1;
+
+}
+
+int ScanfBinaryQuadAnswer (quad _check, int _length) {
+
+    /* octal and hexadecimal requires 4 bytes */
+    char input[33];
+    int sr;
+    quad answer;
+    
+    /* do the scanf */
+    printf("> ");
+    sr = scanf("%32s",input);
+    ClearScanfBuffer();
+    if (sr != 1) return 0;
+
+    /* convert to answer */
+    sr = BinaryToQuad(input,_length,&answer);
+
+    /* return if correct answer */
+    if (sr != 1 || answer != _check) {
+        return 0;
+    }
+    return 1;
+
+}
+
+int DecimalToIEEEByteTest (void) {
+
+    /* declaring variables */
+    quad question = RandomIEEEQuad(4,4);
+    float answer = QuadToFloat(question,4,4);
+    int precision = ComputePrecision(question,4,4);
+
+    /* print question */
+    printf("Assume IEEE754 standard, with 4-bit exponent and 4-bit mantissa.\n"
+        "Convert ");
+    PrintBinaryQuad(question,9);
+    printf(" into decimal form: \n");
+
+    /* scanf answer */
+    if (ScanfFloatAnswer(answer)) {
+        printf("Correct! The answer is %.*f.\n",precision,answer);
+    } else {
+        printf("Wrong! The answer is %.*f.\n",precision,answer);
+    }
+
+    return 0;
+    
+}
+
+int IEEEByteToDecimalTest (void) {
+
+    /* declaring variables */
+    quad answer = RandomIEEEQuad(4,4);
+    float question = QuadToFloat(answer,4,4);
+    int precision = ComputePrecision(answer,4,4);
+
+    /* print question */
+    printf("Assume IEEE754 standard, with 4-bit exponent and 4-bit mantissa.\n"
+        "Convert decimal %.*f into 9-bit binary form: \n",precision,question);
+
+    /* scanf answer */
+    if (ScanfBinaryQuadAnswer(answer,9)) {
+        printf("Correct! The answer is ");
+    } else {
+        printf("Wrong! The answer is ");
+    }
+    PrintBinaryQuad(answer,9);
     printf(".\n");
 
     return 0;
